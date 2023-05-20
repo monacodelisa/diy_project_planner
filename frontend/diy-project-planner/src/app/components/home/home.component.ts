d2df5ee5b8b4dd214d9222d5360b9cd3321e7fa2
Esther White <monacodelisa@gmail.com>
2023-05-20 13:55:07 +0300

    home slider

--- frontend/diy-project-planner/src/app/components/home/home.component.ts
+++ frontend/diy-project-planner/src/app/components/home/home.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('carouselAnimation', [
+      state('0', style({ opacity: 0 })),
+      state('1', style({ opacity: 1 })),
+      transition('* => 1', [
+        animate('1500ms', keyframes([
+          style({ opacity: 0, offset: 0 }),
+          style({ opacity: 1, offset: 1 })
+        ]))
+      ]),
+      transition('1 => 0', [
+        animate('1500ms', keyframes([
+          style({ opacity: 1, offset: 0 }),
+          style({ opacity: 0, offset: 1 })
+        ]))
+      ]),
+    ])
+  ]
 })
 export class HomeComponent {
+  images: string[] = [
+    'assets/images/intro-carousel/1.jpg',
+    'assets/images/intro-carousel/2.jpg',
+    'assets/images/intro-carousel/3.jpg',
+    'assets/images/intro-carousel/4.jpg',
+    'assets/images/intro-carousel/5.jpg'
+  ];
+  currentSlide = 0;
 
+  constructor() {
+    setInterval(() => this.nextSlide(), 5000);
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+  }
 }
